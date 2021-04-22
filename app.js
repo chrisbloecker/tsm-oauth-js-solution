9c2f185ed99abf53a138104858020b4c7e110977
Christopher Blöcker <chrisbloecker@users.noreply.github.com>
2021-04-22 20:16:43 +0200

    removed todos

--- app.js
+++ app.js
@@ -12,13 +12,9 @@ app.set('view engine', 'pug')
 // for making get and post requests
 const axios = require("axios")
 
-// ToDo: fill in your client ID and secret you got from github.
-//       remember, you don't want to commit those strings to your repository, otherwise
-//       everybody who can see your code can use your github account for OAuth
 const client_id     = "c3b8d8b7d1b5d3b87e18"
 const client_secret = "21361fb166fa6340fa16b1b40960e7b1977012c5"
 
-// ToDo: add the the different URLs for using OAuth with github as constants here...
 const redirectURL   = "http://localhost:5000/auth/callback"
 const authorizeURL  = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirectURL}`
 const getTokenURL   = "https://github.com/login/oauth/access_token"
@@ -27,11 +23,6 @@ const getTokenURL   = "https://github.com/login/oauth/access_token"
 // the index page. for this mini app, let's assume that we handle sessions with
 // url parameters. we assume that, if a user is logged in, an OAuth access token
 // is passed to this function in the url.
-//
-// ToDo:
-//  - if we don't get an access token, display a "login with github" button
-//  - if we have an access token, use if to fetch the user info. use it to display
-//    a greeting that includes the user's name, and a logout button
 //
 // there are certainly many ways to do it, but here, let's use a template that
 // contains code for both cases. depending on a (possible undefined) user's name
@@ -59,9 +50,6 @@ app.get("/", (req, res) => {
 
 
 // the auth route.
-//
-// ToDo: redirect the user to github's authorisation url (we could do this also
-//       directly with the login button)
 app.get("/auth", (req, res) => {
   res.redirect(authorizeURL)
 })
@@ -69,10 +57,6 @@ app.get("/auth", (req, res) => {
 
 // the callback url where github redirects the user. we receive a code that we
 // can use to get an access token for the user's account data.
-//
-// ToDo:
-//  - use the code to get an access token
-//  - redirect the user to the start page and pass the access token as a url parameter
 app.get("/auth/callback", (req, res) => {
   code = req.query.code
 
